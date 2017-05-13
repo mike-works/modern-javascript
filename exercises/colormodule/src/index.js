59bda476ef8a271562e502723f9a77d50b4bbb0d
Mike North <michael.l.north@gmail.com>
2017-05-13 19:21:43 +0200

    Prototypes

--- exercises/colormodule/src/index.js
+++ exercises/colormodule/src/index.js
@@ -1,2 +1,16 @@
+import { hexToRgb, rgbToHex } from './color-utils';
+
 export let color = {r: 255, g: 0, b: 0};
 // define the hex property here
+
+Object.defineProperty(color, 'hex', {
+  get() {
+    return rgbToHex(this.r, this.g, this.b);
+  },
+  set(hex) {
+    let { r, g, b } = hexToRgb(hex);
+    this.r = r;
+    this.g = g;
+    this.b = b;
+  }
+});
---
+++ exercises/colortype/src/color-utils.js
@@ -0,0 +1,1 @@
+// Implement your color utils here
