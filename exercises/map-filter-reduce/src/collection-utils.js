75a91174ca15bf213303de2a26d652aab2f2ae03
Mike North <michael.l.north@gmail.com>
2017-05-14 01:05:39 +0200

    implement higher-order functions

---
+++ exercises/map-filter-reduce/src/collection-utils.js
@@ -0,0 +1,53 @@
+/**
+ * Invoke a function for each item in an array
+ *
+ * @export
+ * @param {array} array Array to iterate over
+ * @param {function} func function to invoke
+ * @return {null} nothing
+ */
+export function forEach(array, func) {
+
+}
+
+/**
+ * Create a transformed copy of an array, where
+ * a transformation function is applied to each item
+ *
+ * @export
+ * @param {array} array list of items to transoform
+ * @param {function} func transformation function
+ * @return {array} transformed items
+ */
+export function map(array, func) {
+
+}
+
+/**
+ * Create a filtered array, given an original array and
+ * a filtering function to be invoked on each element.
+ * Only those elements where the filtering function returns
+ * true will be in the returned filtered array.
+ *
+ * @export
+ * @param {array} array original array
+ * @param {function} func filtering function
+ * @return {array} filtered array
+ */
+export function filter(array, func) {
+
+}
+
+/**
+ * Generate a singular value by iterating over an array.
+ * More info: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+ *
+ * @export
+ * @param {any} array original array
+ * @param {any} reducer reducer function
+ * @param {any} initialVal initial value of accumulator
+ * @return {any} ultimate value of the accumulator
+ */
+export function reduce(array, reducer, initialVal) {
+
+}
---
+++ exercises/map-filter-reduce/tests/map-filter-reduce.test.js
@@ -0,0 +1,74 @@
+/* eslint no-extend-native: 0 */
+import * as coll from '../src/collection-utils';
+
+let _oldMap = null;
+let _oldFilter = null;
+let _oldReduce = null;
+
+beforeEach(() => {
+  _oldMap = Array.prototype.map;
+  _oldFilter = Array.prototype.filter;
+  _oldReduce = Array.prototype.reduce;
+
+  Array.prototype.map = () => {
+    throw 'Array.prototype.map is not allowed';
+  };
+  Array.prototype.filter =  () => {
+    throw 'Array.prototype.filter is not allowed';
+  };
+  Array.prototype.reduce =  () => {
+    throw 'Array.prototype.reduce is not allowed';
+  };
+});
+
+afterEach(() => {
+  Array.prototype.map = _oldMap;
+  Array.prototype.filter = _oldFilter;
+  Array.prototype.reduce = _oldReduce;
+});
+
+describe('collection-utils.js exports', () => {
+  test('must export a map function', () => {
+    expect(typeof coll.map).toBe('function');
+  });
+
+  test('must export a reduce function', () => {
+    expect(typeof coll.reduce).toBe('function');
+  });
+
+  test('must export a filter function', () => {
+    expect(typeof coll.filter).toBe('function');
+  });
+
+  test('must export a forEach function', () => {
+    expect(typeof coll.forEach).toBe('function');
+  });
+});
+
+describe('collection-utils#forEach', () => {
+  let c = 0;
+  test('Sum up [1, 2, 3]', () => {
+    coll.forEach([1, 2, 3], (x) => {
+      c += x;
+    });
+    expect(c).toBe(6);
+  });
+});
+
+describe('collection-utils#map', () => {
+  test('Square [1, 2, 3]', () => {
+    expect(coll.map([1, 2, 3], (x) => x * x)).toMatchObject([1, 4, 9]);
+  });
+});
+
+describe('collection-utils#filter', () => {
+  test('filter [1, 2, 3] where x > 2', () => {
+    expect(coll.filter([1, 2, 3], (x) => x > 2)).toMatchObject([3]);
+  });
+});
+
+describe('collection-utils#reduce', () => {
+  test('Sum up [1, 2, 3]', () => {
+    expect(coll.reduce([1, 2, 3], (x, acc) => acc + x, 0)).toBe(6);
+  });
+});
