b687e7fcdbf2739777fc374345b112717967cf75
Michael North <michael.l.north@gmail.com>
2017-08-08 15:55:30 -0700

    Task solution tests

--- exercises/task/tests/task.test.js
+++ exercises/task/tests/task.test.js
@@ -1,12 +1,10 @@
 import * as taskModule from '../src/task.js';
 
-const task = { taskModule };
-
-describe('fib.js exports are correct', () => {
+describe('task.js exports are correct', () => {
   test('getFibSequence export is found', () => {
-    expect(typeof fib.getFibSequence).toBe('function');
+    expect(typeof taskModule.task).toBe('function');
   });
   test('only one thing exported from the module', () => {
-    expect(Object.keys(fib).length).toBe(1);
+    expect(Object.keys(taskModule).length).toBe(1);
   });
 });
