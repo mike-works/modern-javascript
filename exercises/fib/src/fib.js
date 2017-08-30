6571fda8cca496fce0a1733ec0047a12a968c2cb
Michael North <michael.l.north@gmail.com>
2017-08-30 11:54:06 -0700

    fib sequence

--- exercises/fib/src/fib.js
+++ exercises/fib/src/fib.js
@@ -3,10 +3,20 @@
  *
  * @export
  * @param {any} length length of sequence to generate
- * @returns {number[]} fibonacci sequence
+ * @returns {undefined|number[]} fibonacci sequence
  */
-
 export function getFibSequence(length) {
   // TODO: replace this with your implementation
-  return [];
+  let l = parseInt(length, 10);
+  if (l < 0 || isNaN(l)) return;
+  let seq = [];
+  let twoAgo = 1;
+  let oneAgo = 0;
+  for (let i = 0; i < l; i++) {
+    let next = oneAgo + twoAgo;
+    twoAgo = oneAgo;
+    oneAgo = next;
+    seq.push(next);
+  }
+  return seq;
 }
