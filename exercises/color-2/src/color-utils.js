48f17cc0d0b16a083ec6e41a57bb8dc6b8ecd16b
Michael North <michael.l.north@gmail.com>
2017-10-17 15:09:42 +0800

    Exercise 2 solution

--- exercises/color-2/src/color-utils.js
+++ exercises/color-2/src/color-utils.js
@@ -1,2 +1,28 @@
 // Implement your color utils here
+function hexToBinary(h) {
+  return parseInt(h, 16);
+}
 
+export function rgbToHex(r, g, b) {
+  return  [r, g, b]
+    .map(decChan => Math.min(255, Math.max(0, decChan)))
+    .map(decChan => decChan.toString(16))
+    .map(hexCh => hexCh.length === 1 ? `0${hexCh}` : hexCh)
+    .join('');
+}
+
+export function hexToRgb(hex) {
+  let channelLength = hex.length / 3;
+  let hexR = hex.substr(0, channelLength);
+  let hexG = hex.substr(channelLength, channelLength);
+  let hexB = hex.substr(2 * channelLength, channelLength);
+  let r = hexToBinary(channelLength === 2 ? hexR : `${hexR}${hexR}`);
+  let g = hexToBinary(channelLength === 2 ? hexG : `${hexG}${hexG}`);
+  let b = hexToBinary(channelLength === 2 ? hexB : `${hexB}${hexB}`);
+  return {r, g, b}; // {r: 255, g: 100, b: 211}
+}
+
+// export default {
+//   rgbToHex,
+//   hexToRgb
+// };
--- exercises/color-2/src/index.js
+++ exercises/color-2/src/index.js
@@ -1,16 +1,16 @@
 import { hexToRgb, rgbToHex } from './color-utils';
 
-export let color = {r: 255, g: 0, b: 0};
-// define the hex property here
-
-Object.defineProperty(color, 'hex', {
-  get() {
+export let color = {
+  r: 255,
+  g: 0,
+  b: 0,
+  get hex() {
     return rgbToHex(this.r, this.g, this.b);
   },
-  set(hex) {
-    let { r, g, b } = hexToRgb(hex);
+  set hex(hexStr) {
+    let { r, g, b } = hexToRgb(hexStr);
     this.r = r;
     this.g = g;
     this.b = b;
   }
-});
+};
