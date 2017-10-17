a225e5a9eead2a3d8e6b1f529cfd89fef61dbb5a
Michael North <michael.l.north@gmail.com>
2017-10-17 16:07:49 +0800

    Exercise 3 solution

--- exercises/color-3/src/color.js
+++ exercises/color-3/src/color.js
@@ -1,1 +1,26 @@
-// TODO implement Color type
+import { rgbToHex, hexToRgb } from './color-utils';
+
+export function Color(r, g, b) {
+  this.r = r;
+  this.g = g;
+  this.b = b;
+}
+
+Color.prototype = {
+  get hex() {
+    return rgbToHex(this.r, this.g, this.b);
+  },
+  set hex(hexStr) {
+    let { r, g, b } = hexToRgb(hexStr);
+    this.r = r;
+    this.g = g;
+    this.b = b;
+  }
+};
+
+function fromHexConstructor(hexStr) {
+  let { r, g, b } = hexToRgb(hexStr);
+  return new Color(r, g, b);
+}
+
+Color.fromHex = fromHexConstructor;
