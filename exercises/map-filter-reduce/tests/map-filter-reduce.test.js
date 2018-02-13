71e6edf051cfb5f5249346c2b4b738fbd6183933
Michael North <michael.l.north@gmail.com>
2018-02-12 21:38:10 -0800

    No longer forbid Array.map/filter/reduce

--- exercises/map-filter-reduce/tests/map-filter-reduce.test.js
+++ exercises/map-filter-reduce/tests/map-filter-reduce.test.js
@@ -1,32 +1,6 @@
 /* eslint no-extend-native: 0 */
 import * as coll from '../src/collection-utils';
 
-let _oldMap = null;
-let _oldFilter = null;
-let _oldReduce = null;
-
-beforeEach(() => {
-  _oldMap = Array.prototype.map;
-  _oldFilter = Array.prototype.filter;
-  _oldReduce = Array.prototype.reduce;
-
-  Array.prototype.map = () => {
-    throw 'Array.prototype.map is not allowed';
-  };
-  Array.prototype.filter = () => {
-    throw 'Array.prototype.filter is not allowed';
-  };
-  Array.prototype.reduce = () => {
-    throw 'Array.prototype.reduce is not allowed';
-  };
-});
-
-afterEach(() => {
-  Array.prototype.map = _oldMap;
-  Array.prototype.filter = _oldFilter;
-  Array.prototype.reduce = _oldReduce;
-});
-
 describe('collection-utils.js exports', () => {
   test('must export a map function', () => {
     expect(typeof coll.map).toBe('function');
