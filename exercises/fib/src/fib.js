7f56b2c986532691bf5e0fdfb35338c78b4ebac5
Michael North <michael.l.north@gmail.com>
2018-02-13 13:46:41 -0800

    ✅ Exercise 8

--- exercises/fib/src/fib.js
+++ exercises/fib/src/fib.js
@@ -6,7 +6,20 @@
  * @returns {number[]} fibonacci sequence
  */
 
-export function getFibSequence(length) {
+export function getFibSequence(rawLength) {
+  let length = parseInt(rawLength, 10);
+  if (isNaN(length) || length < 0) return undefined;
+  // Here we know length is a number >= 0
+  let older = 1;
+  let old = 0;
+  let values = [];
+
+  for (let i = 0; i < length; i++) {
+    let thisVal = older + old;
+    values.push(thisVal);
+    older = old;
+    old = thisVal;
+  }
   // TODO: replace this with your implementation
-  return [];
+  return values;
 }
