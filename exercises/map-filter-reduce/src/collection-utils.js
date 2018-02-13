9baf5969117801ecb54df8d0cff19eb77c39f408
Michael North <michael.l.north@gmail.com>
2018-02-12 21:34:55 -0800

    ✅ Exercise 5

--- exercises/map-filter-reduce/src/collection-utils.js
+++ exercises/map-filter-reduce/src/collection-utils.js
@@ -1,23 +1,32 @@
+// @ts-check
 /**
  * Invoke a function for each item in an array
  *
  * @export
  * @param {array} array Array to iterate over
  * @param {function} func function to invoke
- * @return {null} nothing
+ * @return {void} nothing
  */
-export function forEach(array, func) {}
+export function forEach(array, func) {
+  for (let i = 0; i < array.length; i++) func(array[i]);
+}
 
 /**
  * Create a transformed copy of an array, where
  * a transformation function is applied to each item
  *
  * @export
- * @param {array} array list of items to transoform
- * @param {function} func transformation function
- * @return {array} transformed items
+ * @param {any[]} array list of items to transoform
+ * @param {function} transformFunc transformation function
+ * @return {any[]} transformed items
  */
-export function map(array, func) {}
+export function map(array, transformFunc) {
+  return reduce(
+    array,
+    (transformed, item) => transformed.concat(transformFunc(item)),
+    []
+  );
+}
 
 /**
  * Create a filtered array, given an original array and
@@ -28,9 +37,18 @@ export function map(array, func) {}
  * @export
  * @param {array} array original array
  * @param {function} func filtering function
- * @return {array} filtered array
+ * @return {any[]} filtered array
  */
-export function filter(array, func) {}
+export function filter(array, func) {
+  return reduce(
+    array,
+    (filtered, item) => {
+      if (func(item)) filtered.push(item);
+      return filtered;
+    },
+    []
+  );
+}
 
 /**
  * Generate a singular value by iterating over an array.
@@ -42,4 +60,10 @@ export function filter(array, func) {}
  * @param {any} initialVal initial value of accumulator
  * @return {any} ultimate value of the accumulator
  */
-export function reduce(array, reducer, initialVal) {}
+export function reduce(array, reducer, initialVal) {
+  let v = initialVal;
+  for (let i = 0; i < array.length; i++) {
+    v = reducer(v, array[i], i, array);
+  }
+  return v;
+}
