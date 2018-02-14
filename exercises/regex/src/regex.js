54d688c11cd2a9b16168a48b5770172095d608a9
Michael North <michael.l.north@gmail.com>
2018-02-14 13:57:43 -0800

    Regex solution

--- exercises/regex/src/regex.js
+++ exercises/regex/src/regex.js
@@ -1,5 +1,5 @@
-export const EMAIL_ADDRESS = /[a-z]/;
+export const EMAIL_ADDRESS = /^[a-zA-Z\-+0-9]+@([a-zA-Z\-+0-9]+\.)+[a-zA-Z\-+0-9]+$/;
 
-export const URL = /[a-z]/;
+export const URL = /^(ftp|https?):\/{2}([A-Za-z0-9\-\_]+\.)+[A-Za-z0-9\-\_]+(\/[a-zA-Z]+)*(\#[a-zA-Z]+)?\/?(\?([a-zA-Z0-9]+\=[a-zA-Z0-9]+)(\&([a-zA-Z0-9]+\=[a-zA-Z0-9]+))*)?$/;
 
-export const PASSWORD_INPUT = /[a-z]/;
+export const PASSWORD_INPUT = /^\<input\s*(?:[a-zA-Z]+(?:\=([\"\'])[a-zA-Z0-9]+\1)?\s*)*\/\>$/;
