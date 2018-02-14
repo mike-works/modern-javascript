63698c5b62f21c4ba1600f10c484cb533fcc32d8
Michael North <michael.l.north@gmail.com>
2018-02-14 14:27:27 -0800

    ES proxy solution

--- exercises/edit-buffer/src/edit-buffer.js
+++ exercises/edit-buffer/src/edit-buffer.js
@@ -1,3 +1,24 @@
 export function createProxyHandler() {
-  return {};
+  let cachedValues = {};
+  const save = target => {
+    for (let i in cachedValues) {
+      if (cachedValues.hasOwnProperty(i)) {
+        target[i] = cachedValues[i];
+      }
+    }
+  };
+  return {
+    get(targ, prop) {
+      // "get property" trap
+      if (prop === 'save') return save.bind(this, targ);
+      return typeof cachedValues[prop] !== 'undefined'
+        ? cachedValues[prop]
+        : targ[prop];
+    },
+    set(targ, prop, newVal) {
+      // "get property" trap
+      cachedValues[prop] = newVal;
+      return newVal;
+    }
+  };
 }
