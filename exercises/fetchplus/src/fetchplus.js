5375745123675727ac36b40f678845795c5615ae
Mike North <michael.l.north@gmail.com>
2018-06-01 12:20:38 -0700

    Exercise 8

--- exercises/fetchplus/src/fetchplus.js
+++ exercises/fetchplus/src/fetchplus.js
@@ -1,4 +1,15 @@
 import { default as _fetch } from 'node-fetch';
 
+const REQUESTS = {};
+
 // Do not change this function signature or the tests will break
-export default function fetchPlus(url, options, fetch = _fetch) {}
+export default function fetchPlus(url, options, fetch = _fetch) {
+  // Begin the process of making a HTTP request
+  let existingRequest = REQUESTS[url];
+  if (!existingRequest) {
+    existingRequest = REQUESTS[url] = fetch(url, options).then(response =>
+      response.json()
+    ); // Start decoding body to JSON
+  }
+  return existingRequest;
+}
