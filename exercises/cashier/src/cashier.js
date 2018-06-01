a52460112a021b6abb8fc32f05027983e008143b
Mike North <michael.l.north@gmail.com>
2018-06-01 10:22:49 -0700

    Exercise 4

--- exercises/cashier/src/cashier.js
+++ exercises/cashier/src/cashier.js
@@ -1,3 +1,20 @@
 export function cashier() {
   // TODO: implement me
+  let items = [];
+  let len = 0;
+  let tot = 0;
+  return {
+    add({ name, price, qty = 1 }) {
+      items.push({ name, price, qty });
+      len += qty;
+      tot += qty * price;
+      return this;
+    },
+    get length() {
+      return len;
+    },
+    get total() {
+      return tot;
+    }
+  };
 }
